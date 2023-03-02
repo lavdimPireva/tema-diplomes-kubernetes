90f64110bab091230392870deb8bb2c3d9901dc7
lavdimPireva <lavdimpireva2@gmail.com>
2023-03-02 18:20:28 +0100

    hello

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,7 +17,7 @@ export default function Home() {
       <main className={styles.main}>
         <div className={styles.description}>
           <p>
-            Welcome to FieK
+            Welcome to FIEK
             <code className={styles.code}> pages/index.tsx</code>
           </p>
           <div>
