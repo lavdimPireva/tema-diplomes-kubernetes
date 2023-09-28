b959aa6569819b663b18fac2ecc549f91f1cac0d
lavdimPireva <lavdimpireva2@gmail.com>
2023-09-29 00:05:44 +0200

    tema-diplomes-changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,7 +17,7 @@ export default function Home() {
       <main className={styles.main}>
         <div className={styles.description}>
           <p>
-            Fiek Fieks
+            Fakulteti i inxhinierise elektrike dhe kompjuterike!
             <code className={styles.code}> pages/index.tsx</code>
           </p>
           <div>
