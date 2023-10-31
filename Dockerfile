18c1339e2c89a5975840d99c0f549500d2d4e8da
lavdimPireva <lavdimpireva2@gmail.com>
2023-10-31 20:04:04 +0100

    changes

--- Dockerfile
+++ Dockerfile
@@ -3,8 +3,6 @@ FROM node:16
 # Create app directory
 WORKDIR /usr/next-js-web-app
 
-# Install dependencies
-# A wildcard is used to ensure both package.json AND package-lock.json are copied
 COPY package*.json /usr/next-js-web-app/
 
 RUN npm install
--- pages/index.tsx
+++ pages/index.tsx
@@ -17,7 +17,7 @@ export default function Home() {
       <main className={styles.main}>
         <div className={styles.description}>
           <p>
-            Fakulteti i inxhinierise elektrike dhe kompjuterike!
+            Fakulteti i inxhinierise elektrike dhe kompjuterike!!
             <code className={styles.code}> pages/index.tsx</code>
           </p>
           <div>
